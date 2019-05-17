2736ca6015ac8c113f25235f16ab40943c024a7f
andre <andre.cfh@gmail.com>
2019-05-17 17:05:56 +0800

    added log4j

--- server/src/main/java/com/example/demo/controller/DemoController.java
+++ server/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import org.jboss.logging.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+	Logger logger = Logger.getLogger(DemoController.class);
+
 	@GetMapping("/")
 	public String index(){
+		logger.info("index");
 		return "This is server index"; 		
 	}
 }
